2667e996c5a8fb93ff0e73aaddf3725d651ef9e2
alexandre cousin <cousin.a@laposte.net>
2019-02-13 16:23:10 +0100

    fin d'exo

---
+++ db/migrate/20190213103358_rename_column_adress_in_table_restaurants_to_address.rb
@@ -0,0 +1,5 @@
+class RenameColumnAdressInTableRestaurantsToAddress < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :restaurants, :adress, :address
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seed strating ...!'
+restaurant1 = Restaurant.new(name: "Epicure", category: "french", address: "75008 Paris", phone_number: "0134756411")
+restaurant1.save!
+restaurant2 = Restaurant.new(name: "La cave de l'insolite", category: "french", address: "75011 Paris", phone_number: "0134752339")
+restaurant2.save!
+restaurant3 = Restaurant.new(name: "Le sourire - Tapas Française", category: "french", address: "75005 Paris", phone_number: "0134752339")
+restaurant3.save!
+restaurant4 = Restaurant.new(name: "L'Oseille", category: "french", address: "75009 Paris", phone_number: "0134752339")
+restaurant4.save!
+restaurant5 = Restaurant.new(name: "Le fossile", category: "french", address: "59000 Lille", phone_number: "0320814545")
+restaurant5.save!
+puts 'Finished!'
